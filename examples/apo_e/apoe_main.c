91d9732f82e93927d1fcbe88e042ef34fe1d64ce
Vojtech Benes <benesv11@cvut.cz>
2025-06-01 17:39:17 +0200

    examples/apo_e: added comments to apoe_main.c, explained algorithms and next steps
    Signed-off-by: Vojtech Benes <benesv11@cvut.cz>

--- examples/apo_e/apoe_main.c
+++ examples/apo_e/apoe_main.c
@@ -14,9 +14,10 @@ void basic_alg();
 int apoe_main(int argc, char *argv[])
 {
     // Initialize subsystems
-    motor_init();       // configure GPIO6/7 as motor outputs
-    servocontrol_init();       // configure PWM on GPIO17 for steering
+    motor_init();
+    servocontrol_init();
     ultrasonic_init();       // configure TRIG/ECHO pins
+    //here will be webserver_init()
     mapping_init();     // prepare data structures
 
     // Center servo and stop motor
@@ -26,6 +27,11 @@ int apoe_main(int argc, char *argv[])
     return 0;
 }
 
+/* basic algorithm to see if the robot moves
+ * robot still hasnt moved an inch...
+ * this function is DEBUG only
+ * main algorithm will be developed after the robot moves - testing is quite hard without it
+ */
 void basic_alg()
 {
     while(1) {
@@ -36,4 +42,15 @@ void basic_alg()
         }
     }
 }
+
+/*
+ * here will be advanced algorithm for robot
+ * robot will scan, store values and move and repeat
+ * if scanned value is too close robot will scan its surroundings if its possible to turn
+ * 1. if its possible it will turn to right or left
+ * 2. if not it will reverse and try step 1. again
+ * this function will be send as a pointer into webserver
+ * this gives the freedom to develop more algorithm and using one based on what we need
+ */
+
 
